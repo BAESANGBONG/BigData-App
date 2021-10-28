9a86a372fe95ad1e3b10bb265ce338f61c602501
Springboot Mustache <qotkdqhd6710@gmail.com>
2021-10-28 11:43:04 +0900

    보고따라해도 잘안되네요....

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import sqlalchemy as db
+
+bream_length = pd.read_csv("bream_length.csv").to_numpy().flatten()
+bream_length = pd.read_csv("bream_length.csv").to_numpy().flatten()
+bream_length = pd.read_csv("bream_length.csv").to_numpy().flatten()
+bream_length = pd.read_csv("bream_length.csv").to_numpy().flatten()
+
+# print(bream_length)
